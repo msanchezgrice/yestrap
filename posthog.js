4277be81ac69679e2bd58c99e53f6dfec3431cfd
Miguel Sanchez-Grice <msanchezgrice@users.noreply.github.com>
2025-12-26 20:53:27 -0600

    Point PostHog to US ingest

--- posthog.js
+++ posthog.js
@@ -2,7 +2,7 @@
   var siteId = "yestrap.com";
   var siteName = "Yes Trap";
   var phKey = "phc_7zxvXFwKp6XHbPHKJISxvqGOlNM2BUwQvUpUkvpPCLs";
-  var apiHost = "https://app.posthog.com";
+  var apiHost = "https://us.i.posthog.com";
 
   (function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once people.unset people.increment people.append people.remove people.group set_group add_group remove_group register register_once unregister opt_out_capturing opt_in_capturing has_opted_out_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group.identify".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)})(document,window.posthog||[]);
 
